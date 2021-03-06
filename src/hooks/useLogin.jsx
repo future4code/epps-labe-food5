15d9c78ec0a1fb984045c2bb893480688c1b75f3
Allison Araujo <allisonaraujo94@outlook.com>
2021-03-06 20:28:19 -0300

    Pequena Correção no useLogin

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,30 +1,32 @@
-import axios from 'axios'
-import { useHistory } from 'react-router'
-import useForm from './useForm'
-import {baseUrl} from '../Constants/url'
+import axios from "axios";
+import { useHistory } from "react-router";
+import useForm from "./useForm";
+import { baseUrl } from "../Constants/url";
 
-export default function Login() {
-    const [form, onChange] = useForm({})
-    const history = useHistory()
+export default function useLogin() {
+  const [form, onChange] = useForm({});
+  const history = useHistory();
 
-    const Login = (event) => {
-        event.preventDefault()
-        const body = {
-            email: form.email,
-            password: form.password
-        }
+  const Login = (event) => {
+    event.preventDefault();
 
-        axios.post(`${baseUrl}/login`, body)
-            .then((response) => {
-                console.log(response.data)
-                localStorage.setItem("token", response.data.token)
-                alert("Logado com exito")
-                history.push('/feed')
-            })
-            .catch((error) => {
-                console.log(error)
-                alert("Erro no login! Favor tente novamente")
-            })
-    }
-return [onChange, form, Login]
+    const body = {
+      email: form.email,
+      password: form.password,
+    };
+
+    axios
+      .post(`${baseUrl}/login`, body)
+      .then((response) => {
+        console.log("Meu Token: ", response.data.token);
+        localStorage.setItem("token", response.data.token);
+        alert("Logado com exito");
+        history.push("/feed");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Erro no login! Favor tente novamente");
+      });
+  };
+  return [onChange, form, Login];
 }
